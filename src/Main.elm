67a48308673566c24d18137f95ff816714599c65
Jorel <jorel.k.ali@gmail.com>
2019-11-30 18:18:58 +0000

    Adds test code

--- src/Main.elm
+++ src/Main.elm
@@ -3,21 +3,30 @@ module Main exposing (..)
 --Main code goes here
 
 import Browser
-import Html exposing (Html, Attribute, div, input, textarea, text, button)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onInput, onClick)
+-- import Html exposing (Html, Attribute, div, input, textarea, text, button)
+-- import Html.Attributes exposing (..)
+-- import Html.Events exposing (onInput, onClick)
 import Interpreter exposing (..)
+
+import Html exposing (Html)
+
+import Element exposing (Element, el, text, row, alignRight, fill, width, rgb255, spacing, centerY, padding)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
 
 -- CONSTANTS
 defaultProgram : String
 defaultProgram = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
 
 -- MAIN
-main =
+-- main : Html msg
+main = 
+  
   Browser.sandbox { 
       init = init
       , update = update
-      , view = view 
+      , view = view
       }
 
 -- MODEL
@@ -44,12 +53,33 @@ update msg model =
 
 -- VIEW
 view : Model -> Html Msg
-view model =
-  div []
-    [ textarea [ placeholder "Brainfuck Program", value model.content, onInput Change, rows 43, cols 80 ] []
-    , div [] [ textarea [] [ text (model.progOutput) ] ]
-    , button [ onClick Update ] [ text "blah" ]
-    ]
+view model = Element.layout [] myRowOfStuff 
+  -- div []
+  --   [ textarea [ placeholder "Brainfuck Program", value model.content, onInput Change, rows 43, cols 80 ] []
+  --   , div [] [ textarea [] [ text (model.progOutput) ] ]
+  --   , button [ onClick Update ] [ text "blah" ]
+  --   ]
+
+
+
+myRowOfStuff : Element msg
+myRowOfStuff =
+    row [ width fill, centerY, spacing 30 ]
+        [ myElement
+        , myElement
+        , el [ alignRight ] myElement
+        ]
+
+
+myElement : Element msg
+myElement =
+    el
+        [ Background.color (rgb255 240 0 245)
+        , Font.color (rgb255 255 255 255)
+        , Border.rounded 3
+        , padding 30
+        ]
+        (text "stylish!")
 
 simpleInterpret : String -> String
 simpleInterpret input = if
