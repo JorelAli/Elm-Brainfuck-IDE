4a98e27fd0db79e358d1af148dc939fcc59ebf83
JorelAli <jorel.k.ali@gmail.com>
2019-12-01 01:00:50 +0000

    Finish brainfuck formatter

--- src/Formatter.elm
+++ src/Formatter.elm
@@ -1,10 +1,21 @@
 module Formatter exposing (..)
 
 format : String -> String
-format program = formatIterator '#' (strip program) ""
+format program = formatIterator '#' 0 (strip program) "" |> cleanup
 
-formatIterator : Char -> String -> String -> String
-formatIterator prevChar restOfProgram currentOutput = 
+cleanup : String -> String
+cleanup semiFormatted =
+  let
+    a = String.replace "+++++" "+++++ " semiFormatted
+    b = String.replace "-----" "----- " a
+    c = String.replace "<<<<<" "<<<<< " b
+    d = String.replace ">>>>>" ">>>>> " c
+    e = String.replace "\n\n" "\n" d
+  in 
+    e
+
+formatIterator : Char -> Int -> String -> String -> String
+formatIterator prevChar indentation restOfProgram currentOutput = 
   let
     currentChar = 
       case String.uncons restOfProgram of 
@@ -17,39 +28,46 @@ formatIterator prevChar restOfProgram currentOutput =
             Just (letter2, rest2) -> letter2
             Nothing -> '#'
         Nothing -> '#'
+    result = formatChar indentation prevChar currentChar nextChar
   in
     case currentChar of
       '#' -> currentOutput
       _ ->
-        formatIterator currentChar (
+        formatIterator currentChar (Tuple.second result) (
           case String.uncons restOfProgram of
             Just (x, xs) -> xs
-            Nothing -> restOfProgram) (currentOutput ++ formatChar prevChar currentChar nextChar)
+            Nothing -> restOfProgram) (currentOutput ++ Tuple.first result)
   
-formatChar : Char -> Char -> Char -> String
-formatChar prev cur next =
+indentationToString : Int -> String
+indentationToString level = 
+  case level of 
+    0 -> ""
+    _ -> "    " ++ indentationToString (level - 1)
+
+formatChar : Int -> Char -> Char -> Char -> (String, Int)
+formatChar indentation prev cur next =
   case cur of
     '>' -> case prev of
-      '>' -> ">"
-      '<' -> ">"
-      '#' -> "    >"
-      _ -> "\n" ++ "    " ++ ">"
+      '>' -> (">", indentation)
+      '<' -> (">", indentation)
+      '#' -> (indentationToString indentation ++ ">", indentation)
+      _ -> ("\n" ++ indentationToString indentation ++ ">", indentation)
     '<' -> case prev of
-      '>' -> "<"
-      '<' -> "<"
-      '#' -> "    <"
-      _ -> "\n    <"
+      '>' -> ("<", indentation)
+      '<' -> ("<", indentation)
+      '#' -> (indentationToString indentation ++ "<", indentation)
+      _ -> ("\n" ++ indentationToString indentation ++ "<", indentation)
     '+' -> if prev == '<' || prev == '>'
-      then " +"
-      else "+"
+      then (" +", indentation)
+      else ("+", indentation)
     '-' -> if prev == '<' || prev == '>'
-      then " -"
-      else "-"
-    '.' -> "    ."
-    ',' -> "    ,"
-    '[' -> "\n    ["
-    ']' -> "\n    ]"
-    _ -> ""
+      then (" -", indentation)
+      else ("-", indentation)
+    '.' -> (indentationToString indentation ++ ".\n", indentation)
+    ',' -> (indentationToString indentation ++ ",\n", indentation)
+    '[' -> ("\n" ++ indentationToString indentation ++ "[", indentation + 1) --"\n    [" --indent++
+    ']' -> ("\n" ++ indentationToString (indentation - 1) ++ "]", indentation - 1) --"\n    ]" --indent--
+    _ -> ("", indentation)
 
 strip : String -> String
 strip str = 
